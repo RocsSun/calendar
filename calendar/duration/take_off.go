95b3a527125463a1df8dc668d0060f275e19f15c
RocsSun <710989028@qq.com>
2022-06-23 13:26:43 +0800

    修复请假/调休时长计算错误.

--- calendar/duration/take_off.go
+++ calendar/duration/take_off.go
@@ -24,8 +24,6 @@ type CountTime struct {
 	isOffset    bool      // 标记是否处理过
 }
 
-//var dateMap map[string]bool
-
 // countDays 计算天数
 func (c *CountTime) countDays() float64 {
 	var days = 0.0
@@ -34,8 +32,8 @@ func (c *CountTime) countDays() float64 {
 			days++
 		}
 	}
-
-	return days
+	res, _ := decimal.NewFromFloat(days).Round(2).Float64()
+	return res
 }
 
 // countHours 计算请假的时间。
@@ -48,7 +46,8 @@ func (c *CountTime) countHours() float64 {
 // EffectTime 计算有效时间。
 func (c *CountTime) EffectTime() float64 {
 	c.offset()
-	return c.countDays() + c.countHours()
+	res, _ := decimal.NewFromFloat(c.countDays() + c.countHours()).Round(2).Float64()
+	return res
 }
 
 func (c *CountTime) offset() {
@@ -62,35 +61,36 @@ func (c *CountTime) offset() {
 
 	// 处理请假开始时间。
 	// 处理请假开始时间在下午下班之后的情况。重置时间为第二天的早上上班时间。
-	if c.Start.Hour() >= c.PmEnd.Hour() && c.Start.Minute() >= c.PmEnd.Minute() {
-		c.offsetStart = c.Start.Add(
-			time.Duration(24-c.Start.Hour()+c.AmStart.Hour()) * time.Hour).Add(
-			time.Duration(c.AmStart.Minute()-c.Start.Minute()) * time.Minute)
+	if c.offsetStart.Hour()*60+c.offsetStart.Minute() >= c.PmEnd.Hour()*60+c.PmEnd.Minute() {
+		c.offsetStart = c.offsetStart.Add(
+			time.Duration(
+				24*60-c.offsetStart.Hour()*60-c.offsetStart.Minute()+c.AmStart.Minute()+c.AmStart.Hour()*60,
+			) * time.Minute,
+		)
 	}
 
 	// 处理请假开始时间在早上上班之前的，重置时间为当天早上上班时间。
-	if c.Start.Hour() <= c.AmStart.Hour() && c.Start.Minute() < c.AmStart.Minute() {
-		c.offsetStart = c.Start.Add(
-			time.Duration(c.AmStart.Hour()-c.Start.Hour()) * time.Hour).Add(
-			time.Duration(c.AmStart.Minute()-c.Start.Minute()) * time.Minute)
+	if c.offsetStart.Hour()*60+c.offsetStart.Minute() < c.AmStart.Hour()*60+c.AmStart.Minute() {
+		c.offsetStart = c.offsetStart.Add(
+			time.Duration((c.AmStart.Hour()-c.Start.Hour())*60+c.AmStart.Minute()-c.Start.Minute()) * time.Minute,
+		)
 	}
 
 	// 处理请假开始时间在在早上下班之后（含），下午上班之前的（含），重置请假开始时间为当天下午上班时间。
-	if c.Start.Hour() >= c.AmEnd.Hour() && c.Start.Minute() >= c.AmEnd.Minute() && c.Start.Hour() < c.PmStart.Hour() && c.Start.Minute() < c.PmStart.Minute() {
-		c.offsetStart = c.Start.Add(
-			time.Duration(c.PmStart.Hour()-c.Start.Hour()) * time.Hour,
-		).Add(
-			-time.Duration(c.PmStart.Minute()-c.Start.Minute()) * time.Minute,
+	if c.offsetStart.Hour()*60+c.offsetStart.Minute() >= c.AmEnd.Hour()*60+c.AmEnd.Minute() &&
+		c.offsetStart.Hour()*60+c.offsetStart.Minute() <= c.PmStart.Hour()*60+c.PmStart.Minute() {
+		c.offsetStart = c.offsetStart.Add(
+			time.Duration(c.PmStart.Hour()*60+c.PmStart.Minute()-(c.offsetStart.Hour()*60+c.offsetStart.Minute())) * time.Minute,
 		)
 	}
 
 	// 处理请假结束时间。
 	// 处理请假结束时间在上班之前的。重置的前一天的下午下班时间。
-	if c.End.Hour() <= c.AmStart.Hour() && c.End.Minute() <= c.Start.Minute() {
-		c.offsetEnd = c.End.Add(
-			time.Duration(-24+c.PmEnd.Hour()-c.End.Hour()) * time.Hour,
-		).Add(
-			time.Duration(c.PmEnd.Minute()-c.End.Minute()) * time.Minute,
+	if c.offsetEnd.Hour()*60+c.offsetEnd.Minute() <= c.AmStart.Hour()*60+c.AmStart.Minute() {
+		c.offsetEnd = c.offsetEnd.Add(
+			time.Duration(
+				(c.PmEnd.Hour()-24-c.offsetEnd.Hour())*60+c.PmEnd.Minute()-c.offsetEnd.Minute(),
+			) * time.Minute,
 		)
 
 		// 更新日期。
@@ -100,30 +100,27 @@ func (c *CountTime) offset() {
 		c.PmEnd = c.PmEnd.Add(-24 * time.Hour)
 	}
 	// 处理请假结束时间在下班之后的，重置为当天的下午下班时间。
-	if c.End.Hour() >= c.PmEnd.Hour() && c.End.Minute() > c.PmEnd.Minute() {
+	if c.offsetEnd.Hour()*60+c.offsetEnd.Minute() > c.PmEnd.Hour()*60+c.PmEnd.Minute() {
 		c.offsetEnd = c.PmEnd
 	}
 	// 处理请假结束时间在早上下班之后（含），下午上班之前的（含），重置为当天早上下班时间。
-	if (c.End.After(c.AmEnd) || c.End.Equal(c.AmEnd)) && (c.End.Before(c.PmStart) || c.End.Equal(c.PmStart)) {
+	if (c.offsetEnd.After(c.AmEnd) || c.offsetEnd.Equal(c.AmEnd)) && (c.offsetEnd.Before(c.PmStart) || c.offsetEnd.Equal(c.PmStart)) {
 		c.offsetEnd = c.AmEnd
 	}
 
-	// 处理请假开始时间
+	// 处理请假开始时间大于等于请假结束时间，跨天请假的情况。
 	if c.offsetStart.Before(c.offsetEnd) && c.offsetStart.Hour() >= c.offsetEnd.Hour() {
-		c.balance = float64(c.PmEnd.Hour()-c.Start.Hour()) + float64((c.PmEnd.Minute()-c.Start.Minute())/60) + c.balance
-		// 前置一天。
+		c.balance = float64(c.PmEnd.Hour()-c.Start.Hour()) + float64(c.PmEnd.Minute()-c.Start.Minute())/60 + c.balance
 		c.offsetStart = c.offsetStart.Add(
-			time.Duration(24 - c.offsetStart.Hour() + c.AmStart.Hour()),
-		).Add(
-			time.Duration(c.AmStart.Minute()-c.offsetStart.Minute()) * time.Minute,
+			time.Duration((24-c.offsetStart.Hour()+c.AmStart.Hour())*60+c.AmStart.Minute()-c.offsetStart.Minute()) * time.Minute,
 		)
 	}
 
 	// 处理请假结束时间在早上下班之前，早上上班之后（含），下午上班之后前，下午下班之前（含），减去中午的休息时间。
-	if float64(c.offsetStart.Hour())+float64(c.offsetStart.Minute()/60) >= float64(c.AmStart.Hour())+float64(c.AmStart.Minute()/60) &&
-		float64(c.offsetStart.Hour())+float64(c.offsetStart.Minute()/60) < float64(c.AmEnd.Hour())+float64(c.AmEnd.Minute()/60) &&
-		float64(c.offsetEnd.Hour())+float64(c.offsetEnd.Minute()/60) > float64(c.PmStart.Hour())+float64(c.PmStart.Minute()/60) &&
-		float64(c.offsetEnd.Hour())+float64(c.offsetEnd.Minute()/60) <= float64(c.PmEnd.Hour())+float64(c.PmEnd.Minute()/60) {
+	if c.offsetStart.Hour()*60+c.offsetStart.Minute() >= c.AmStart.Hour()*60+c.AmStart.Minute() &&
+		c.offsetStart.Hour()*60+c.offsetStart.Minute() < c.AmEnd.Hour()*60+c.AmEnd.Minute() &&
+		c.offsetEnd.Hour()*60+c.offsetEnd.Minute() > c.PmStart.Hour()*60+c.PmStart.Minute() &&
+		c.offsetEnd.Hour()*60+c.offsetEnd.Minute() <= c.PmEnd.Hour()*60+c.PmEnd.Minute() {
 		//c.balance = c.balance - float64(c.PmStart.Hour()-c.AmEnd.Hour()) + float64(c.PmStart.Minute()-c.AmEnd.Minute())/60
 		c.balance = c.balance - c.PmStart.Sub(c.AmEnd).Minutes()/60
 	}
--- calendar/duration/take_off_test.go
+++ calendar/duration/take_off_test.go
@@ -1,12 +1,12 @@
 package duration_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/RocsSun/calendar/calendar/duration"
 )
 
+// TestEffectTimes 测试不跨天的正常请假
 func TestEffectTimes(t *testing.T) {
 	start := "2022-01-05 08:30"
 	end := "2022-01-05 17:00"
@@ -21,6 +21,7 @@ func TestEffectTimes(t *testing.T) {
 	}
 }
 
+// TestNewCountTime1 测试请假某几个小时。
 func TestNewCountTime1(t *testing.T) {
 	start := "2022-01-05 15:30"
 	end := "2022-01-05 17:00"
@@ -34,6 +35,7 @@ func TestNewCountTime1(t *testing.T) {
 	}
 }
 
+// TestNewCountTime3 测试请假半天
 func TestNewCountTime3(t *testing.T) {
 	start := "2022-01-05 13:30"
 	end := "2022-01-05 17:00"
@@ -48,6 +50,7 @@ func TestNewCountTime3(t *testing.T) {
 	}
 }
 
+// TestNewCountTime4 测试请假半天，除去中午休息时间。
 func TestNewCountTime4(t *testing.T) {
 	start := "2022-01-05 08:30"
 	end := "2022-01-05 13:30"
@@ -56,8 +59,121 @@ func TestNewCountTime4(t *testing.T) {
 	ps := "13:00"
 	pe := "17:00"
 	tim := duration.EffectTimes(start, end, as, ae, ps, pe)
-	fmt.Println(tim)
+
 	if tim != 0.5 {
+		t.Error("failed!")
+	}
+}
+
+// TestNewCountTime5 测试跨天。
+func TestNewCountTime5(t *testing.T) {
+	start := "2022-01-04 13:00"
+	end := "2022-01-05 13:00"
+	as := "08:30"
+	ae := "11:30"
+	ps := "13:00"
+	pe := "17:00"
+	tim := duration.EffectTimes(start, end, as, ae, ps, pe)
+
+	if tim != 1 {
+		t.Error("failed!")
+	}
+}
+
+// TestNewCountTime6 测试跨天请假开始时间的小时数大于请假结束时间的小时数
+func TestNewCountTime6(t *testing.T) {
+	start := "2022-01-04 14:00"
+	end := "2022-01-06 13:00"
+	as := "08:30"
+	ae := "11:30"
+	ps := "13:00"
+	pe := "17:00"
+	tim := duration.EffectTimes(start, end, as, ae, ps, pe)
+
+	if tim != 1.86 {
+		t.Error("failed!")
+	}
+}
+
+// TestNewCountTime7 测试请假结束时间是第二天早上上班之前。
+func TestNewCountTime7(t *testing.T) {
+	start := "2022-01-04 17:00"
+	end := "2022-01-06 08:00"
+	as := "08:30"
+	ae := "11:30"
+	ps := "13:00"
+	pe := "17:00"
+	tim := duration.EffectTimes(start, end, as, ae, ps, pe)
+	if tim != 1 {
+		t.Error("failed!")
+	}
+}
+
+// TestNewCountTime8 测试请假开始在早上上班之前，请假结束时间是第二天早上上班之前。
+func TestNewCountTime8(t *testing.T) {
+	start := "2022-01-04 07:40"
+	end := "2022-01-06 08:00"
+	as := "08:30"
+	ae := "11:30"
+	ps := "13:00"
+	pe := "17:00"
+	tim := duration.EffectTimes(start, end, as, ae, ps, pe)
+
+	if tim != 2 {
+		t.Error("failed!")
+	}
+}
+
+// TestNewCountTime9 测试请假开始时间在中午休息时间段的。
+func TestNewCountTime9(t *testing.T) {
+	start := "2022-01-04 11:30"
+	end := "2022-01-06 18:00"
+	as := "08:30"
+	ae := "11:30"
+	ps := "13:00"
+	pe := "17:00"
+	tim := duration.EffectTimes(start, end, as, ae, ps, pe)
+
+	if tim != 2.57 {
+		t.Error("failed!")
+	}
+}
+
+// TestNewCountTime10 测试重复修正时间。
+func TestNewCountTime10(t *testing.T) {
+	start := "2022-01-04 11:30"
+	end := "2022-01-06 18:00"
+	as := "08:30"
+	ae := "11:30"
+	ps := "13:00"
+	pe := "17:00"
+	cli := duration.NewCountTime(start, end, as, ae, ps, pe)
+	if cli == nil {
+		t.Error("init cli failed! ")
+	}
+
+	tim := cli.EffectTime()
+	if tim != 2.57 {
+		t.Error("failed!")
+	}
+
+	tim = cli.EffectTime()
+	if tim != 2.57 {
+		t.Error("failed!")
+	}
+}
+
+// TestNewCountTime11 测试跨年。
+func TestNewCountTime11(t *testing.T) {
+	start := "2021-12-31 08:30"
+	end := "2022-01-05 13:30"
+	as := "08:30"
+	ae := "11:30"
+	ps := "13:00"
+	pe := "17:00"
+	tim := duration.EffectTimes(start, end, as, ae, ps, pe)
+
+	if tim != 2.5 {
 		t.Error("failed!")
 	}
 }
